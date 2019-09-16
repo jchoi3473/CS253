aac73336145023e6506cc02168122e2e6ff64919
jchoi3473 <jchoi3473@yahoo.com>
2019-09-15 21:17:56 -0400

    Queue Commit

---
+++ src/main/java/edu/emory/cs/queue/TernaryHeap.java
@@ -0,0 +1,78 @@
+package edu.emory.cs.queue;
+
+import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.lang.Math;
+
+public class TernaryHeap <T extends Comparable<T>> extends AbstractPriorityQueue<T>{
+    private List<T> keys;
+    public TernaryHeap(Comparator<T> comparator){ //constructor with parameter
+        super(comparator);
+        keys = new ArrayList<>();
+        keys.add(null); //initialize the first element in the ArrayList as null
+    }
+    public TernaryHeap(){ //constructor without paramete
+        this(Comparator.naturalOrder());
+    }
+
+    public void add(T key){//add method to add keys into the ArrayList. After the addition, it uses swim method
+        keys.add(key);
+        swim(size());
+    }
+    public T remove(){//remove and return a max key in the arraylist.
+        if (isEmpty()) return null;
+        Collections.swap(keys, 1, size());
+        T max = keys.remove(size());
+        sink(1);//this will find the new highest value and will store
+        return max;
+    }
+    public int size(){
+        return keys.size()-1;
+    }
+    //this is the sinking method that gets used during remove
+    public void sink(int k){
+        //unlike binary, (k*3)-1 is used to set i as the left most item in the heap
+        for (int i = (k * 3)-1; i <= size(); k = i, i = (i*3)-1) {
+            //this if statement is to determine whether the middle element of the three items in the children is the biggest. if middle one is the biggest,
+            //then we add 1 in order to switch i to the middle element. Because we want to examine keys.get(i+2), its very important to set i<size()-2
+            //unless, it will throw array out of bounds error.
+            if (i < size()-2 && comparator.compare(keys.get(i), keys.get(i+1)) < 0&&comparator.compare(keys.get(i+2), keys.get(i+1))<0) i++;
+            //same logic applies here. This time, the right most element gets examined to verify whether its the biggest element or not.
+            else if (i < size()-2 && comparator.compare(keys.get(i), keys.get(i+2)) < 0&&comparator.compare(keys.get(i+1), keys.get(i+2))<0) i=i+2;
+            //when there are 2 or less elements, that same concept used from binary heap can be used.
+            else if (i < size() && comparator.compare(keys.get(i), keys.get(i+1)) < 0) i++;
+            //when k becomes equal to or bigger than i, we break the loop since no swap or searching for bigger element is required.
+            if (comparator.compare(keys.get(k), keys.get(i)) >= 0) break;
+            Collections.swap(keys, k, i);
+        }
+    }
+
+    public void swim(int k){
+        //a rounded value of any one of 3 children/3.0 equals the value of its parent.
+        //because division of children/3 only yields integer, usage of double is needed in this case.
+        //After the division, it gets converted into int type so that it matches its parent type(int)
+        while (1 < k && comparator.compare(keys.get((int)Math.round(k/3.0)), keys.get(k)) < 0) {
+           //from while loop, if newly added value is proven to have larger keys, it then uses swap.
+            Collections.swap(keys, (int)Math.round(k/3.0), k);
+            //after swap, it needs to find the new parent. K becomes the new parent after the calculation and while loop continues
+            k = (int)Math.round(k/3.0);
+        }
+    }
+
+     /*
+    public void swim(int k){
+        //a rounded value of any one of 3 children/3.0 equals the value of its parent.
+        //because division of children/3 only yields integer, usage of double is needed in this case.
+        //After the division, it gets converted into int type so that it matches its parent type(int)
+        while (1 < k && comparator.compare(keys.get((int)((k/3.0)+(1.0/3.0))), keys.get(k)) < 0) {
+            //from while loop, if newly added value is proven to have larger keys, it then uses swap.
+            Collections.swap(keys, (int)((k/3.0)+(1.0/3.0)), k);
+            //after swap, it needs to find the new parent. K becomes the new parent after the calculation and while loop continues
+            k = (int)((k/3.0)+(1.0/3.0));
+        }
+    }
+*/
+}
--- src/main/java/edu/emory/cs/utils/Utils.java
+++ src/main/java/edu/emory/cs/utils/Utils.java
@@ -7,6 +7,19 @@ public class Utils {
         return beginIndex + (endIndex - beginIndex) / 2;
     }
 
+    static public int[] getRandomIntArray(Random rand, int size) {
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++)
+            array[i] = rand.nextInt();
+
+        return array;
+    }
+
+    static public double log2(int i) {
+        return Math.log(i) / Math.log(2);
+    }
+
     static public void main(String[] args) {
         System.out.println(getMiddleIndex(0, 10));
     }
